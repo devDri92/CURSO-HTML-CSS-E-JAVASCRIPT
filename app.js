26c6e63a92b32894f4594690936ef05be919bc0a
Andriele Silva <andrieles1992@gmail.com>
2024-05-15 20:07:07 -0300

    Add files via upload
    Neste curso, vou ter acesso às informações fundamentais para trabalhar com o desenvolvimento para a Web. Vou aprender a usar HTML, CSS e JavaScript para criar sites ou aplicativos da Web que se parecem e funcionam como aplicativos nativos, mas que são executados em meu navegador de Internet.

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if(className == ("light-theme")) {
+        this.textContent = "Dark";
+        
+        console.log('current class name: ' + className);
+    }
+    else {
+        this.textContent = "Light";
+    }
+});
+
+ 
